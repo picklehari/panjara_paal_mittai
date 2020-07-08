6f239b9562d72b9a953480227813777d5adec9c7
picklehari <harirajmanghat123@gmail.com>
2020-07-08 17:37:17 +0530

    Chat app Sample

---
+++ index.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var socket = require('socket.io');
+
+// App setup
+var app = express();
+var server = app.listen(4000, function(){
+    console.log('listening for requests on port 4000,');
+});
+
+// Static files
+app.use(express.static('public'));
+users = []
+// Socket setup & pass server
+var io = socket(server);
+io.on('connection', (socket) => {
+
+    console.log('made socket connection', socket.id);
+    users.push(socket.id)
+    // Handle chat event
+    socket.on('chat', function(data){
+        i = users[0]
+        // console.log(data);
+        //io.sockets[i].emit('chat', data);
+        io.to(i).emit('chat',data)
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        i = users[0]
+        io.to(i).emit('typing', data);
+    });
+
+});
