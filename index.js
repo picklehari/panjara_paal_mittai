7058ca021e23b0b369963e07b268ef8e016bc397
picklehari <harirajmanghat123@gmail.com>
2020-07-08 19:17:39 +0530

    chatroom

--- index.js
+++ index.js
@@ -16,12 +16,18 @@ io.on('connection', (socket) => {
 
     console.log('made socket connection', socket.id);
     users.push(socket.id)
+    if(users.length > 1){
+        io.sockets.connected[users[0]].join("roomA");
+        io.sockets.connected[users[1]].join("roomA");
+    }
+    else{
+        io.sockets.connected[users[0]].join("roomA");
+    }
     // Handle chat event
     socket.on('chat', function(data){
-        i = users[0]
         // console.log(data);
         //io.sockets[i].emit('chat', data);
-        io.to(i).emit('chat',data)
+        io.to("roomA").emit('chat',data)
     });
 
     // Handle typing event
